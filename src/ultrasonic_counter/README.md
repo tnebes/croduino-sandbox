f6385a8f2ccd69042c3ef3fd946f5a4f297aab9e
tnebes <47086978+tnebes@users.noreply.github.com>
2020-12-25 15:11:35 +0100

    Created a program that counts how many times something has passed in front of the ultrasonic sensor

---
+++ src/ultrasonic_counter/README.md
@@ -0,0 +1,16 @@
+# Name #
+Self-adjusting ultrasonic counter
+
+## Uses ##
+Ultrasonic Ranging Module HC - SR04
+1 LED
+Croduino Basic3
+
+## Use ##
+Counts instances of things passing in front of ultrasonic sensor
+
+## Input ##
+Ultrasonic Ranging Module HC - SR04
+
+## Output ##
+Int representing how many times the sensor was triggered
---
+++ src/ultrasonic_counter/ultrasonic_counter.ino
@@ -0,0 +1,85 @@
+/*
+ * Upon activation, adjusts the distance to obstacle.
+ * Marked by blinking LED.
+ * If none is found, assume it's infinite.
+ * Once adjustment is completed, stop blinking.
+ * 
+ * from this point on, every X loops transmits the counter
+ * over serial.
+ * 
+ */
+
+// digital pins
+const int triggerPin = 2;
+const int echoPin = 3;
+const int pingLEDPin = 4;
+const float delayTime = 0.1;
+long max_distance;
+int counter = 0, old_counter = 0;
+
+void setup() {
+    // setting up pins.
+    pinMode(triggerPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+    pinMode(pingLEDPin, OUTPUT);
+
+    // establishing serial connection to main
+    Serial.begin(9600);
+    // self-adjustment
+    adjust();
+}
+
+int adjust() {
+    int startupDelay = 3000;
+    //indicator light on.
+    digitalWrite(pingLEDPin, HIGH);
+    delay(startupDelay);
+    digitalWrite(pingLEDPin, LOW);
+    
+    max_distance = determineMaxDistance();
+}
+
+long determineMaxDistance() {
+    long time = 0;
+    long sum = 0;
+    int numberOfMeasurements = 10;
+
+    for (int i = 0; i < numberOfMeasurements; i++) {
+        digitalWrite(pingLEDPin, HIGH);
+        sum += detect();
+        delay(100);
+        digitalWrite(pingLEDPin, LOW);
+        delay(100);
+    }
+    return sum / numberOfMeasurements;
+}
+
+long detect() {
+    onePing();
+    return pulseIn(echoPin, HIGH);
+}
+
+void onePing() {
+    // send a pulse
+    digitalWrite(triggerPin, HIGH);
+    // wait for ping to be sent
+    delay(delayTime);
+    // one ping finished
+    digitalWrite(triggerPin, LOW);
+}
+
+void loop() {
+    if (detect() < max_distance * 0.9) {
+        counter++;
+        digitalWrite(pingLEDPin, HIGH);
+        while (detect() < max_distance * 0.9) {
+            delay(50);
+        }
+        digitalWrite(pingLEDPin, LOW);
+    }
+    if (counter != old_counter) {
+        old_counter++;
+        Serial.print(counter);
+        Serial.print("\n");
+    }
+}
